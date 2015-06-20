d73b3807fe52daf9dcb520ead92a3d0b0ddf194f
Ragnar Rova <ragnar.rova@gmail.com>
2015-06-21 01:19:39 +0200

    Show check output

--- start_containers.sh
+++ start_containers.sh
@@ -5,5 +5,5 @@ HOST2=$(docker run --name xdb2 -d $IMAGE)
 HOST3=$(docker run --name xdb3 -d $IMAGE)
 docker run --link xdb1:xdb1 --link xdb2:xdb2 --link xdb3:xdb3 \
            galera-repro-fab /w/env/bin/fab --fabfile=/w/fabfile.py \
-           -u root -p docker -H xdb1,xdb2,xdb3 setup
+           -u root -p docker -H xdb1,xdb2,xdb3 setup check
 docker rm -f $HOST1 $HOST2 $HOST3
