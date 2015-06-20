019a5fd911d98fd81a7f630af659945d5a87590c
Ragnar Rova <ragnar.rova@gmail.com>
2015-06-21 00:37:14 +0200

    Name the containers

--- start_containers.sh
+++ start_containers.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 IMAGE=galera-repro
-HOST1=$(docker run -d $IMAGE)
-HOST2=$(docker run -d $IMAGE)
-HOST3=$(docker run -d $IMAGE)
+HOST1=$(docker run --name xdb1 -d $IMAGE)
+HOST2=$(docker run --name xdb2 -d $IMAGE)
+HOST3=$(docker run --name xdb3 -d $IMAGE)
 IP1=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $HOST1)
 IP2=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $HOST2)
 IP3=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $HOST3)
 ./env/bin/fab -u root -p docker -H $IP1,$IP2,$IP3 setup
 ./env/bin/fab -u root -p docker -H $IP1,$IP2,$IP3 check
 docker rm -f $HOST1 $HOST2 $HOST3
-exit
